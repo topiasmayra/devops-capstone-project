cf5f9b2f02127c4f996d087f27334a662fc648c3
topiasmayra <topias.mayra@gmail.com>
2023-07-03 08:31:49 -0400

    Added Read account approute

--- service/routes.py
+++ service/routes.py
@@ -83,7 +83,8 @@ def create_accounts():
     def test_get_account_not_found(self):
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/0")
-        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND) 
+        #comment
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
